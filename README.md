46085819d3cfdfee7e863668c13620a88b3ae79d
Alexander Niebuhr <alexander@nbhr.io>
2025-06-01 10:32:32 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# cf-vite-jsonwebtoken
+# octokit-graphql-types
+
+As you can see in `src/index.ts` the variable `fromCore` is typed correctly, hower as you can see `fromWebhookCallback` is typed as `unknown`.
+This looks off to me, and I really need to use the `octokit` instance available in the webhook callback, to get it authenticated.
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,70 @@
-import jwt from 'jsonwebtoken';
+import { App } from "@octokit/app";
+import { Octokit } from "@octokit/core";
+import { paginateRest } from "@octokit/plugin-paginate-rest";
+import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
+import { retry } from "@octokit/plugin-retry";
 
-export default {
-  fetch() {
-    const decoded = jwt.verify(
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c",
-       "your-256-bit-secret", {
-			algorithms: ['HS256'],
-			complete: false,
-		});
-    console.log(decoded)
-    return new Response(`Running in ${navigator.userAgent}!`);
+export const octokitFromCore = new Octokit()
+
+const fromCore = octokitFromCore.graphql({
+  query: `query($owner: String!, $repo: String!, $number: Int!) {
+      repository(owner: $owner, name: $repo) {
+        pullRequest(number: $number) {
+          closingIssuesReferences(first: 50) {
+            nodes {
+              number
+              title
+              url
+            }
+          }
+        }
+      }
+    }`,
+  owner: "",
+  repo: "",
+  number: "",
+});
+
+
+export const app = new App({
+  appId: "",
+  privateKey: "",
+  webhooks: {
+    secret: "",
   },
-};
+  Octokit: Octokit.plugin(restEndpointMethods, paginateRest, retry),
+});
+
+app.webhooks.on("pull_request.opened", async ({ payload, octokit }) => {
+  const fromWebhookCallback = await octokit.graphql({
+    query: `query($owner: String!, $repo: String!, $number: Int!) {
+      repository(owner: $owner, name: $repo) {
+        pullRequest(number: $number) {
+          closingIssuesReferences(first: 50) {
+            nodes {
+              number
+              title
+              url
+            }
+          }
+        }
+      }
+    }`,
+    owner: payload.repository.owner.login,
+    repo: payload.repository.name,
+    number: payload.pull_request.number,
+  });
+});
+export default {
+  async fetch(request): Promise<Response> {
+    await app.webhooks.verifyAndReceive({
+      id: request.headers.get("x-github-delivery") || "",
+      name: request.headers.get("x-github-event") || "",
+      signature: request.headers.get("x-hub-signature-256") || "",
+      payload: await request.text(),
+    });
+    return new Response(`{ "ok": true }`, {
+      headers: { "content-type": "application/json" },
+    });
+  },
+} satisfies ExportedHandler<Env>;
